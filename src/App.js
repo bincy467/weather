8db599723237a1153c8bc9951a8351e859a126c2
bincy467 <sbincy467@gmail.com>
2024-03-31 15:06:54 +0530

    Commit message for submodule changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+
+import React, { useEffect, useState } from 'react';
+import axios from "axios"
+
 import './App.css';
 
 function App() {
+
+const[weatherdata,setweatherdata]=useState({
+
+  main:{},
+  weather:[{}]
+});
+
+useEffect (()=>{
+
+  async function getdata(){
+    const Api= "https://api.openweathermap.org/data/2.5/weather?q=Palakkad,india&appid=cd508e904800802440254761b832d5de&units=metric";
+
+
+try{
+  let response=await axios.get(Api)
+  console.log(response.data);
+  setweatherdata(response.data)
+}catch(err){
+  console.log(err);
+}
+
+  }
+  getdata()
+},[])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='home-container'>
+      <div className='main-container'>
+        <div className='card'>
+          <h1>{weatherdata.name}</h1>
+          <h1>{`${Math.round(weatherdata.main.temp)}°C`}</h1>
+          <h1>{weatherdata.weather[0].main}</h1>
+          <h6>{weatherdata.weather[0].description}</h6>
+          
+
+        </div>
+      </div>
+     
     </div>
   );
 }
